27b5a398959a530622c7ee66e2f116ac09544087
dlfud <99ysl@naver.com>
2022-11-17 13:41:26 +0900

    recoil, layout만듬, AuthRoutes로 로그인유저검증

--- front/src/routes/Home.jsx
+++ front/src/routes/Home.jsx
@@ -1,23 +1,17 @@
-import { useEffect, useState } from "react";
-import { Link, useLocation, useNavigate } from "react-router-dom";
+import { useEffect } from "react";
+import { Link, Navigate } from "react-router-dom";
+import { useRecoilState } from "recoil";
+import Layout from "../layouts/Layout";
+import { userState } from "../recoil";
 
 const Home = () => {
-  const navigate = useNavigate();
-  const location = useLocation();
-
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
+  const [user, setUser] = useRecoilState(userState);
 
   useEffect(() => {}, []);
   return (
-    <div className="flex gap-4">
-      <div>
-        <Link to="/join">회원가입</Link>
-      </div>
-      <div>
-        <Link to="/login">로그인</Link>
-      </div>
-    </div>
+    <Layout>
+      <div>home {user && `안녕하세요 ${user.username}님`}</div>;
+    </Layout>
   );
 };
 
--- front/src/routes/Login.jsx
+++ front/src/routes/Login.jsx
@@ -1,57 +1,68 @@
 import axios from "axios";
 import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { useRecoilState } from "recoil";
+import Layout from "../layouts/Layout";
+import { userState } from "../recoil";
 import { url } from "../utils/BackendUrl";
 
 const Login = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const [user, setUser] = useRecoilState(userState);
+  const navigate = useNavigate();
+
   return (
-    <div>
-      <form
-        onSubmit={async (e) => {
-          e.preventDefault();
-          try {
-            const data = await axios({
-              url: `${url}/api/v1/user/login`,
-              method: "POST",
-              data: { email, password },
-            });
-            setEmail("");
-            setPassword("");
-            alert("로그인 성공!");
-          } catch (e) {
-            console.log(e);
-            alert("로그인 실패!");
-          }
-        }}
-        className="flex flex-col w-60 items-start p-4"
-      >
-        <div>로그인</div>
-        <div className="mt-4">이메일을 입력해 주세요</div>
-        <input
-          type="email"
-          className="w-full border-4 border-black"
-          placeholder="Email"
-          value={email}
-          onChange={(e) => {
-            setEmail(e.target.value);
+    <Layout>
+      <div>
+        <form
+          onSubmit={async (e) => {
+            e.preventDefault();
+            try {
+              const data = await axios({
+                url: `${url}/api/v1/user/login`,
+                method: "POST",
+                data: { email, password },
+              });
+              setEmail("");
+              setPassword("");
+              setUser(data.data);
+              navigate("/");
+              alert("로그인 성공!");
+            } catch (e) {
+              console.log(e);
+              alert("로그인 실패!");
+            }
           }}
-        />
-        <div className="mt-4">비밀번호을 입력해 주세요</div>
-        <input
-          type="password"
-          className="w-full border-4 border-black"
-          placeholder="password"
-          value={password}
-          onChange={(e) => {
-            setPassword(e.target.value);
-          }}
-        />
-        <button type="submit" className="btn btn-sm ml-auto mt-4">
-          전송
-        </button>
-      </form>
-    </div>
+          className="flex flex-col w-60 items-start p-4"
+        >
+          <div>로그인</div>
+          <div className="mt-4">이메일을 입력해 주세요</div>
+          <input
+            type="email"
+            className="w-full border-4 border-black"
+            placeholder="Email"
+            value={email}
+            onChange={(e) => {
+              setEmail(e.target.value);
+            }}
+          />
+          <div className="mt-4">비밀번호을 입력해 주세요</div>
+          <input
+            type="password"
+            className="w-full border-4 border-black"
+            placeholder="password"
+            value={password}
+            onChange={(e) => {
+              setPassword(e.target.value);
+            }}
+          />
+          <button type="submit" className="btn btn-sm ml-auto mt-4">
+            전송
+          </button>
+        </form>
+      </div>
+    </Layout>
   );
 };
 
